75039256084bff21be3971bffc2949ff0bdbc823
Akashraghav2003 <akash.si8273@gmail.com>
2025-01-21 15:10:53 +0530

    add someother files

---
+++ feature21-01-2025/practice_problem/NamingConversation.cs
@@ -0,0 +1,12 @@
+using System;
+
+class NamingConversation{ // using pascal case for classname
+	
+	public static void Main (){
+		int num1 = 5; // for variable we use camele case
+		int num2 = 10;
+		
+		Console.WriteLine("sum of two digit: "+(num1+num2)); // for method we use pascal case in that WriteLine function 
+	}
+	
+}
