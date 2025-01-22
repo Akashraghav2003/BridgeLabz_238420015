9890c17f6e9fc169a0e600748e5aca9e4e09d3ab
Akashraghav2003 <akash.si8273@gmail.com>
2025-01-22 12:18:28 +0530

    add  SwapNumbers.cs file on behalf of lab2 problem7 solution

---
+++ SwapNumbers.cs
@@ -0,0 +1,26 @@
+using System; // Import System namespace
+
+namespace Assignment2{
+	
+    class SwapNumbers{
+		
+        static void Main()
+        {
+            // enter the first number from user
+            Console.Write("Enter first number: ");
+            int number1 = int.Parse(Console.ReadLine()); // Convert input to integer
+
+            //  enter the second number from user
+            Console.Write("Enter second number: ");
+            int number2 = Convert.ToInt64(Console.ReadLine()); // Convert input to integer
+
+            // Swap numbers using a temporary variable
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            // Display the swapped numbers
+            Console.WriteLine("The swapped numbers are " + number1 + " and " + number2);
+        }
+    }
+}
