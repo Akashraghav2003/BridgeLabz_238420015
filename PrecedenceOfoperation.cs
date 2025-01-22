35e1f062d657fce5ed3aa41a6a2a6a7dd5635c7f
Akashraghav2003 <akash.si8273@gmail.com>
2025-01-22 11:22:01 +0530

    add PrecedenceOfoperation.cs file as solution of lab 2 problem solution 1

---
+++ PrecedenceOfoperation.cs
@@ -0,0 +1,29 @@
+using System; // Import System namespace
+
+namespace Assignment2{
+	
+    class PrecedenceOfOperation{
+		
+        static void Main(String[] args){
+			
+            // Take input from user
+            Console.Write("Enter value for a: ");
+            int a = int.Parse(Console.ReadLine()); // Convert input to integer
+
+            Console.Write("Enter value for b: ");
+            int b = Convert.ToInt32(Console.ReadLine()); // Convert input to integer
+
+            Console.Write("Enter value for c: ");
+            int c = Convert.ToInt32(Console.ReadLine()); // Convert input to integer
+
+            // Perform integer operations based on precedence
+            int result1 = a + b * c; // Multiplication has higher precedence than addition
+            int result2 = a * b + c; // Multiplication first, then addition
+            int result3 = c + a / b; // Division first, then addition
+            int result4 = a % b + c; // Modulus first, then addition
+
+            // Display the results using concatenation
+            Console.WriteLine("The results of Int Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+        }
+    }
+}
