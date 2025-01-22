c71d8617d2511fae90237d38fe898b5e3bbb0df2
Akashraghav2003 <akash.si8273@gmail.com>
2025-01-22 08:04:53 +0530

    resolve error

--- feature21-01-2025/practice_problem/NamingConversation.cs
+++ feature21-01-2025/practice_problem/NamingConversation.cs
@@ -1,12 +1,12 @@
-using System;
+//using System;
 
-class NamingConversation{ // using pascal case for classname
+//class NamingConversation{ // using pascal case for classname
 	
-	public static void Main (){
-		int num1 = 5; // for variable we use camele case
-		int num2 = 10;
+	//public static void Main (){
+		//int num1 = 5; // for variable we use camele case
+		//int num2 = 10;
 		
-		Console.WriteLine("sum of two digit: "+(num1+num2)); // for method we use pascal case in that WriteLine function 
-	}
+		//Console.WriteLine("sum of two digit: "+(num1+num2)); // for method we use pascal case in that WriteLine function 
+	//}
 	
-}
+//}
