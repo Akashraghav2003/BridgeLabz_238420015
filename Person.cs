21f2cee2c6872c30294319730879f4d01b7d1cba
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-03 14:07:55 +0530

    to show details of person

---
+++ Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    internal class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public long mobNo { get; set; }
+
+        public Person()
+        {
+            name = "akash singh";
+            age = 21;
+            mobNo = 8273932418;
+        }
+
+        public Person(Person other)
+        {
+            name = other.name;
+            age = other.age;
+            mobNo = other.mobNo;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}, mobNo: {mobNo}");
+        }
+
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Display();
+
+            Person person1 = new Person(person);
+            person1.Display();
+        }
+    }
+}
