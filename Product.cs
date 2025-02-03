5c22ccbd0463cec5d7d83df5a5b8410d04b14b61
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-03 14:08:43 +0530

    it is solution of inventory system

---
+++ Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    internal class Product
+    {
+        public string productName { get; set; }
+        public int price { get; set; }
+        public static int totalProduct = 0;
+
+        public Product(string productName, int price)
+        {
+            this.productName = productName;
+            this.price = price;
+            totalProduct++;
+        }
+
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine($"Product Name: {productName}, Price: {price}");
+        }
+
+        public void DisplayTotalProducts()
+        {
+            Console.WriteLine($"Total product: {totalProduct}");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the product name: ");
+            string productName = Console.ReadLine();
+
+            Console.WriteLine("Enter the price: ");
+            int price = Convert.ToInt32(Console.ReadLine());
+
+            Product product = new Product(productName, price);
+
+            product.DisplayProductDetails();
+
+            Product product1 = new Product("samsung", 100);
+            product1.DisplayProductDetails();
+            product1.DisplayTotalProducts();
+        }
+    }
+}
