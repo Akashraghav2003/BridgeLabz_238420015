cda8bcc5900e4f548b34956cd58a6b5cba0b93b6
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-07 11:29:33 +0530

    Add the bank account project

---
+++ BankAnd AccountHolders/Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAnd_AccountHolders
+{
+    internal class Bank
+    {
+        
+        private List<Customer> customers = new List<Customer>();
+
+        
+
+        public void OpenAccount()
+        {
+            Console.WriteLine("Enter the user name");
+            string accountHolderName = Console.ReadLine();
+            Console.WriteLine("Enter the Account Number");
+            String accountNumber  = Console.ReadLine();
+            Console.WriteLine("Enter the initial balane to summit");
+            int balance = Convert.ToInt32(Console.ReadLine());
+
+            Customer customer = new Customer (accountHolderName, accountNumber, balance);
+            customers.Add(customer);
+            customer.CustomerSwitchCase();
+
+        }
+
+        public void Deposit()
+        {
+            Console.WriteLine("write the balance to deposite");
+            int deposite = Convert.ToInt32(Console.ReadLine());
+
+            if(deposite > 0)
+            {
+                balance += deposite;
+            }
+            else
+            {
+                Console.WriteLine("Amount is invalid. ");
+            }
+        }
+
+       
+
+    }
+}  
---
+++ BankAnd AccountHolders/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAnd_AccountHolders
+{
+    internal class Customer
+    {
+        private String accountHolderName;
+        private String accountnumber;
+        private int balance { get; set; }
+
+        public Customer(String accountHolderName, String accountnumber, int balance)
+        {
+            this.accountHolderName = accountHolderName;
+            this.accountnumber = accountnumber;
+            this.balance = balance;
+        }
+        public void ViewBalance()
+        {
+            Console.WriteLine($"Total Balance of Account {balance}");
+        }
+
+        public void CustomerSwitchCase()
+        {
+            Console.WriteLine("Choose the operation");
+            Console.WriteLine("1.View the bank balance \n 2.Exits");
+            int operation = Convert.ToInt32( Console.ReadLine() );
+
+            while (operation > 0)
+            {
+                switch (operation)
+                {
+                    case 1:
+                        ViewBalance();
+                        break;
+                    case 2:
+                        return;
+                    default:
+                        Console.WriteLine("Choose the write operation.");
+                        break;
+                }
+
+            }
+        }
+    }
+}
