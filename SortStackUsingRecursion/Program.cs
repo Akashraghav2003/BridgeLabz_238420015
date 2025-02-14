a191e9eb42ecae91ac06ef8b825ed97f2976d779
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-14 17:25:32 +0530

    SortStackusingRecursion add file to sort the stack by using recursion

---
+++ SortStackUsingRecursion/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace SortStackUsingRecursion
+{
+    class Program
+    {
+        int item;
+
+        public void GetDetails()
+        {
+            Console.WriteLine("Enetr the item");
+            item = Convert.ToInt32(Console.ReadLine());
+    
+        }
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            Program program = new Program();
+
+            while (true)
+            {
+                Console.WriteLine("Choose the operation");
+                Console.WriteLine(" 1.Push\n 2.Pop \n 3.IsEmpty \n 4.Peek\n 5.Sort \n 6.Display \n 7.Exits");
+
+                int operation = Convert.ToInt32(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case 1:
+                        program.GetDetails();
+                        stack.Push(program.item);
+                        break;
+                    case 2:
+                        Console.WriteLine("pop item is: " + stack.Pop());
+                        break;
+                    case 3:
+                        if (stack.isEmpty())
+                        {
+                            Console.WriteLine("stack is empty");
+                        }
+                        else
+                        {
+                            Console.WriteLine("stack is not Empty");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("peek element is:  "+stack.Peek());
+                        break;
+                    case 5:
+                        stack.Sort();
+                        break;
+                    case 6:
+                        stack.Display();
+                        break;
+                    case 7:
+                        return;
+                        
+                    default:
+                        Console.WriteLine("Choose the corect opeartion");
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ SortStackUsingRecursion/Stack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortStackUsingRecursion
+{
+    internal class Stack
+    {
+        private Node top;
+
+        public Stack()
+        {
+            top = null;
+        }
+
+        public void Push(int item)
+        {
+            Node newItem = new Node(item);
+
+            newItem.Next = top;
+            top = newItem;
+
+            Console.WriteLine($"{item} is push");  
+        }
+
+        public int Pop()
+        {
+            if( top == null )
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            int value = top.Data;
+            top = top.Next;
+            Console.WriteLine($"{value} is poped");
+            return value;
+        }
+
+        public int Peek()
+        {
+            if( top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+
+            return top.Data;
+        }
+
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+
+        public void Sort()
+        {
+            if (!isEmpty())
+            {
+                int temp = Pop();
+                Sort();
+                SortedInserted(temp);
+            }
+            
+        }
+
+        public void SortedInserted(int temp)
+        {
+            if(isEmpty() || Peek() <= temp){
+                Push(temp);
+            }else
+            {
+                int item = Pop();
+                SortedInserted(temp);
+                Push(item);
+            }
+        }
+        public void Display()
+        {
+            Node Current = top;
+            while( Current != null )
+            {
+                Console.WriteLine(Current.Data+" ");
+                Current = Current.Next;
+            }
+        }
+    }
+}
