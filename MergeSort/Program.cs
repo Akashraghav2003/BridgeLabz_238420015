e45c4a51d66aec56db04575c095ca71b54c1b76b
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-15 10:18:03 +0530

    Merge Sort add file to find the solution of Merge sort

---
+++ MergeSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using MergeSort;
+
+namespace mergeSort
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of  book ");
+            int books = Convert.ToInt32(Console.ReadLine());
+
+            double[] arr = new double[books];
+
+
+            for (int i = 0; i < books; i++)
+            {
+                Console.WriteLine($"Enter the {i + 1}. Book price: ");
+                arr[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Merge merge = new Merge();
+            merge.Display(arr);
+
+            Merge.MergeSort(arr, 0, books-1);
+            Console.WriteLine("Books price after merge sort: ");
+            merge.Display(arr);
+
+
+        }
+    }
+}
