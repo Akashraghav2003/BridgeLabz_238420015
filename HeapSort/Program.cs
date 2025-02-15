3dbb5b30e8dbc5a3ce58e200a9ec3ee4fe0dd792
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-15 10:16:42 +0530

    Heap Sort add file to find the solution of heap sort

---
+++ HeapSort/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of job applicants:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write($"Enter the salary of applicant {i + 1}: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nUnsorted Salaries:");
+            Heap heap = new Heap();
+            heap.Display(arr);
+
+            // Correct function call
+            Heap.HeapSortAlgo(arr);
+
+            Console.WriteLine("\nSalaries after Heap Sorting:");
+            heap.Display(arr);
+        }
+    }
+}
