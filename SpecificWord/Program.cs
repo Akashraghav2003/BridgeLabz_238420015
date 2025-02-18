c57354fa941aa9e536f59a8be2c326a8906a9b77
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 12:00:01 +0530

    SpecificWord add to find specific word in the text

---
+++ SpecificWord/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SpecificWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of sentence");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            string [] arr = new string[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Enter the {i+1} Sentence: ");
+                arr[i] = Console.ReadLine();
+            }
+
+            Console.Write("Enetr the specific word to find: ");
+            string word = Console.ReadLine();
+
+            SpecificWords specific = new SpecificWords();
+            
+
+
+
+            Console.WriteLine($"Frist sentence in which {word} present:" +specific.Specific(arr, word));
+        }
+    }
+}
---
+++ SpecificWord/SpecificWords.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificWord
+{
+    internal class SpecificWords
+    {
+        public string Specific(string[] sentences, string specificWord)
+        {
+            int len = sentences.Length;
+            string result = " ";
+
+            foreach (string sentence in sentences)
+            {
+                string []  words = sentence.Split(" ");
+                bool flag = false;
+                foreach (string word in words)
+                {
+                    if(word == specificWord)
+                    {
+                        flag = true;
+                        result = sentence;
+                        break;
+                    }
+                }
+                if (flag) break;
+            }
+            return result;
+        }
+    }
+}
