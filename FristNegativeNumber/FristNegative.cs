1ba0fe92540af79e6a30a68610dadef50ddd83f7
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 11:51:54 +0530

    FristNeagtiveNumber add file to find frist negative number by binary search

---
+++ FristNegativeNumber/FristNegative.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FristNegativeNumber
+{
+    internal class FristNegative
+    {
+        public int Negative(int[] arr)
+        {
+            int length = arr.Length;
+            int result = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    result = arr[i];
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FristNegativeNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FristNegativeNumber
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of array");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[size];  
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Enter the {i} element: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            FristNegative fristNegative = new FristNegative();  
+            
+
+            Console.WriteLine("Frist negative element of array :"+ fristNegative.Negative(arr));
+        }
+    }
+}
