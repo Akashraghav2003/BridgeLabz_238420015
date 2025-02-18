5483637e479f4ad2c9039b3e32dd6ac05dc15b1a
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 11:57:55 +0530

    RotatedSortedArray add file to find starting point of array by using binary search

---
+++ Rotated Sorted Array/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Rotated_Sorted_Array
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the rotated sorted array:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter the {i} element: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            RotationPointFinder rotationPointFinder = new RotationPointFinder();    
+            int rotationIndex = rotationPointFinder.FindRotationPoint(arr);
+            Console.WriteLine("The rotation point (index of the smallest element) is: " + rotationIndex);
+        }
+    
+    }
+}
---
+++ Rotated Sorted Array/RotationPointFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotated_Sorted_Array
+{
+    internal class RotationPointFinder
+    {
+        public  int FindRotationPoint(int[] arr)
+        {
+            int left = 0, right = arr.Length - 1;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+
+                // If mid element is greater than the last element, rotation point is on the right side
+                if (arr[mid] > arr[right])
+                    left = mid + 1;
+                else
+                    right = mid; // Rotation point could be mid, so include mid in search
+            }
+
+            return left; // Left will point to the smallest element (rotation point)
+        }
+    }
+}
