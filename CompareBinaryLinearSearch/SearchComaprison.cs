28f38566c61cf272edd2ea941c325ac982a18bb6
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 12:07:56 +0530

    CompaerBinaryLinerSearch add file to compare the Liner and binary search by tricks

---
+++ CompareBinaryLinearSearch/SearchComaprison.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareBinaryLinearSearch
+{
+    internal class SearchComaprison
+    {
+        public static int LinearSearch(int[] arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                    return i; // Return index if found
+            }
+            return -1; // Target not found
+        }
+
+        // Binary Search: Efficient search on sorted data
+        public static int BinarySearch(int[] arr, int target)
+        {
+            int left = 0, right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == target)
+                    return mid; // Return index if found
+                else if (arr[mid] < target)
+                    left = mid + 1; // Search in the right half
+                else
+                    right = mid - 1; // Search in the left half
+            }
+            return -1; // Target not found
+        }
+    }
+}
