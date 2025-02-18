43644203dc09aaf91677d9caaf6af7940e03e632
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 11:55:20 +0530

    RemoveDuplicates add file to remove duplicates by using StringBuilder

---
+++ RemoveDuplicate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace RemoveDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the strig");
+            StringBuilder sb = new StringBuilder(Console.ReadLine());
+
+            Remove remove = new Remove();
+
+            Console.WriteLine("String After remove duplicates: " + remove.RemoveDuplicates(sb));
+        }
+    }
+}
---
+++ RemoveDuplicate/Remove.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace RemoveDuplicate
+{
+    internal class Remove
+    {
+        public StringBuilder RemoveDuplicates(StringBuilder str)
+        {
+            int newLength = 1;
+            for (int i = 1; i < str.Length; i++)
+            {
+                bool isDuplicate = false;
+
+                for (int j = 0; j < newLength; j++) // Only check within newLength
+                {
+                    if (str[i] == str[j]) // If duplicate found, mark it
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate) // If not duplicate, add to new position
+                {
+                    str[newLength] = str[i];
+                    newLength++;
+                }
+            }
+
+            // Trim excess characters
+            while (str.Length > newLength)
+            {
+                str.Length--;
+            }
+
+            return str;
+        }
+    }
+}
