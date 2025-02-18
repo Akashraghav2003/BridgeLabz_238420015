4d9b8511cbf8b98df4a119906a666d289271fb4b
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 12:17:35 +0530

    ComparisonStringBuilderPerformance add file to comparison the string builder

---
+++ CompareStringBuilderPerformance/Performancechecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareStringBuilderPerformance
+{
+    internal class Performancechecker
+    {
+        public long PerformanceCheckStringBulider(long iterations, string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            long start, end;
+            start = DateTime.Now.Ticks;
+            for (int i = 0; i < iterations; i++)
+            {
+                sb.Append(str);
+            }
+            end = DateTime.Now.Ticks;
+            return end - start;
+        }
+
+        public long PerformanceCheckString(long iterations, string str)
+        {
+            string newStr = "";
+            long start, end;
+            start = DateTime.Now.Ticks;
+            for (int i = 0; i < iterations; i++)
+            {
+                newStr += str;
+            }
+            end = DateTime.Now.Ticks;
+            return end - start;
+        }
+
+    }
+}
---
+++ CompareStringBuilderPerformance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CompareStringBuilderPerformance 
+{ 
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Performancechecker performanceTrack = new Performancechecker();
+            Console.WriteLine("Enter number of Iterations to check time: ");
+            long iterations = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the string: ");
+            string str = Console.ReadLine();
+
+            long time1 = performanceTrack.PerformanceCheckStringBulider(iterations, str);
+            long time2 = performanceTrack.PerformanceCheckString(iterations, str);
+            Console.WriteLine($"Time taken By StringBuilder: {time1} ticks \nTime taken By String: {time2} ticks");
+        }
+    }
+}
