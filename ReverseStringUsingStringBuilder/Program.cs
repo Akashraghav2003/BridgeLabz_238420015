9426164f914830954dbabddcfef1055ab82ac374
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 11:56:42 +0530

    ReverseStringUsingStringBuilder add file to reverse string by using stringBuilder

---
+++ ReverseStringUsingStringBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace ReverseStringUsingStringBuilder
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the strig");
+            StringBuilder sb = new StringBuilder(Console.ReadLine());
+
+            ReverseString reverseString = new ReverseString();
+
+            Console.WriteLine("Reversed string is :" + reverseString.Reverse(sb));
+        }
+    }
+}
---
+++ ReverseStringUsingStringBuilder/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStringUsingStringBuilder
+{
+    internal class ReverseString
+    {
+        
+        public StringBuilder Reverse(StringBuilder sb)
+        {
+            int i = 0, j = sb.Length - 1;
+            while (i < j)
+            {
+                char temp = sb[i];
+                sb[i++] = sb[j];
+                sb[j--] = temp;
+                
+            }
+            return sb;
+
+        }
+    }
+}
