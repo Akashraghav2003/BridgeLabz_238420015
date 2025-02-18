0d2fb451f439394f42a0671827568a778c7f3d96
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 11:54:17 +0530

    PeakElementArray add file to find peak element number by binary search

---
+++ PeakElementInArray/FindPeakElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakElementInArray
+{
+    internal class FindPeakElement
+    {
+        public static int PeakElement(int[] arr)
+        {
+            int left = 0, right = arr.Length - 1;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+
+                // If mid element is greater than the next element, the peak is on the left side (or mid itself)
+                if (arr[mid] > arr[mid + 1])
+                    right = mid;
+                else
+                    left = mid + 1; // Otherwise, the peak is on the right side
+            }
+
+            return left; // 'left' will point to the peak element
+        }
+    }
+}
---
+++ PeakElementInArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PeakElementInArray
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the array elements:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter the {i} element: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int peakIndex = FindPeakElement.PeakElement(arr);
+            Console.WriteLine("The peak element is at index: " + peakIndex + " (Value: " + arr[peakIndex] + ")");
+        }
+    
+    }
+}
