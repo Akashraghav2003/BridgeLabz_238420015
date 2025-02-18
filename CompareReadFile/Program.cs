4147b104e3f14fff33afd2d4cb38975a175a0f33
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-18 12:09:02 +0530

    CompaerBinaryLinerSearch add file to Compaer the read file by streamReader and filestream by tricks

---
+++ CompareReadFile/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CompareReadFile
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\akash gla university\\capgemini problem\\feature15-02-2025\\sample.txt";
+
+            Console.WriteLine("Reading large file: " + filePath);
+
+            long streamReaderTime = ReadingComparison.ReadUsingStreamReader(filePath);
+            Console.WriteLine($"StreamReader Time: {streamReaderTime} ms");
+
+            long fileStreamTime =ReadingComparison.ReadUsingFileStream(filePath);
+            Console.WriteLine($"FileStream Time: {fileStreamTime} ms");
+        }
+    }
+}
---
+++ CompareReadFile/ReadingComparison.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareReadFile
+{
+    internal class ReadingComparison
+    {
+        public static long ReadUsingStreamReader(string filePath)
+        {
+            long start = DateTime.Now.Ticks;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                while (reader.Read() != -1) { } // Read character by character
+            }
+            long end = DateTime.Now.Ticks;
+            return (end - start) / 10000; // Convert ticks to milliseconds
+        }
+
+        // Read file using FileStream (Byte-based reading)
+        public static long ReadUsingFileStream(string filePath)
+        {
+            long start = DateTime.Now.Ticks;
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[4096]; // 4KB buffer for efficient reading
+                while (fs.Read(buffer, 0, buffer.Length) > 0) { }
+            }
+            long end = DateTime.Now.Ticks;
+            return (end - start) / 10000; // Convert ticks to milliseconds
+        }
+
+    }
+}
