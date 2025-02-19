c8e7e65709f3234332cfc2b03bded0c2d8d9c8a7
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:21:37 +0530

    GenerateBinaryNumbersUsingQueue add file to found the binary number

---
+++ GenerateBinaryNumbersUsingQueue/BinaryNumberGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateBinaryNumbersUsingQueue
+{
+    internal class BinaryNumberGenerator
+    {
+        public static List<string> GenerateBinaryNumbers(int N)
+        {
+            List<string> result = new List<string>();
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue("1"); // Start with "1"
+
+            for (int i = 0; i < N; i++)
+            {
+                string binary = queue.Dequeue();
+                result.Add(binary);
+
+                // Generate next two binary numbers
+                queue.Enqueue(binary + "0");
+                queue.Enqueue(binary + "1");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GenerateBinaryNumbersUsingQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenerateBinaryNumbersUsingQueue
+{
+	class Program
+	{
+        static void Main()
+        {
+            Console.Write("Enter N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            List<string> binaryNumbers =BinaryNumberGenerator.GenerateBinaryNumbers(N);
+
+            Console.WriteLine("Binary Numbers:");
+            Console.WriteLine("{" + string.Join(", ", binaryNumbers) + "}");
+        }
+    }
+}
