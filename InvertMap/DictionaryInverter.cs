18e3b81461b00da70adb536dc361520d41d21910
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:24:42 +0530

    InvertMap add file to find the dictionay of the dictionary

---
+++ InvertMap/DictionaryInverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertMap
+{
+    internal class DictionaryInverter
+    {
+        public static Dictionary<V, List<K>> InvertDictionary<K, V>(Dictionary<K, V> inputDict)
+        {
+            Dictionary<V, List<K>> invertedDict = new Dictionary<V, List<K>>();
+
+            foreach (var pair in inputDict)
+            {
+                if (!invertedDict.ContainsKey(pair.Value))
+                {
+                    invertedDict[pair.Value] = new List<K>();
+                }
+                invertedDict[pair.Value].Add(pair.Key);
+            }
+
+            return invertedDict;
+        }
+    }
+}
---
+++ InvertMap/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace InvertMap
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> input = new Dictionary<string, int>
+        {
+            { "A", 1 },
+            { "B", 2 },
+            { "C", 1 }
+        };
+
+            Dictionary<int, List<string>> output =DictionaryInverter.InvertDictionary(input);
+
+            Console.WriteLine("Inverted Dictionary:");
+            foreach (var pair in output)
+            {
+                Console.WriteLine($"{pair.Key}: [{string.Join(", ", pair.Value)}]");
+            }
+        }
+    }
+}
