c65d248dacb30b92f082f7c1a7a3d36118dc1eb6
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:26:53 +0530

    ReverseList add file to reverse the linked list and list

---
+++ ReverseList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace ReverseList
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            ReverseListAndLinkedList rev = new ReverseListAndLinkedList();  
+
+            ArrayList list = new ArrayList();
+            Console.WriteLine("Enter the element of arrayList:");
+            string [] inputArray = Console.ReadLine().Split(' ');
+            foreach (string input in inputArray)
+            {
+                if (int.TryParse(input, out int value))
+                {
+                    list.Add(value);
+                }
+            }
+
+            rev.Reverse(list);
+            Console.WriteLine("ArrayList after reverse: ");
+            Console.WriteLine("Reverse ArrayList: "+ string.Join(",", list.ToArray()));
+
+            LinkedList<int> ints = new LinkedList<int>();
+            Console.WriteLine("Enter the element of LinkedList (with space ):");
+            string[] linkedInput = Console.ReadLine().Split(' ');
+            foreach (string s  in linkedInput)
+            {
+                if(int.TryParse(s, out int value))
+                {
+                    ints.AddLast(value);
+                }
+            }
+            rev.ReverseLinkedList(ints);
+            Console.WriteLine("Reversed LinkedList: ");
+            foreach(var item in ints)
+            {
+                Console.Write(item+" ");
+            }
+
+        }
+    }
+}
---
+++ ReverseList/ReverseListAndLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseList
+{
+    internal class ReverseListAndLinkedList
+    {
+        public void Reverse(ArrayList list)
+        {
+            int left = 0;
+            int right = list.Count - 1;
+
+            while (left < right)
+            {
+                object temp = list[left];
+                list[left] = list[right];
+                list[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        public void ReverseLinkedList(LinkedList<int> list)
+        {
+            if (list.Count <= 1) return;
+
+            LinkedListNode<int> prev = null;
+            LinkedListNode<int> current = list.First;
+
+            while (current != null)
+            {
+                LinkedListNode<int> next = current.Next;
+                current.List.Remove(current);
+                list.AddFirst(current);
+                current = next;
+            }
+
+        }
+    }
+}
