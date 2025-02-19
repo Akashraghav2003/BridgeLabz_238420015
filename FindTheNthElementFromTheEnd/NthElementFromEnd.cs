611dbe2809b8470bec3c1bf1a5c32c09d3386850
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:19:37 +0530

    FindTheNthElementFromTheEnd add file to find nth element from last of linked list

---
+++ FindTheNthElementFromTheEnd/NthElementFromEnd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheNthElementFromTheEnd
+{
+    internal class NthElementFromEnd
+    {
+        public string ElementFromEnd(LinkedList<string> linkedList, int index)
+        {
+            LinkedListNode<string> slow = linkedList.First;
+            LinkedListNode<string> fast = linkedList.First;
+
+            for (int i = 0; i< index; i++)
+            {
+                if (fast == null) return null;
+
+                fast = fast.Next;
+                
+            }
+
+            while (fast != null)
+            {
+                fast = fast.Next;
+                
+                slow = slow.Next;
+                
+
+            }
+
+            return slow.Value;
+
+        }
+    }
+}
---
+++ FindTheNthElementFromTheEnd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace FindTheNthElementFromTheEnd
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            NthElementFromEnd nthElementFromEnd = new NthElementFromEnd();
+            LinkedList<string> list = new LinkedList<string>();
+
+            Console.WriteLine("Enter elements of LinkedList (space-separated):");
+            string[] input = Console.ReadLine().Split();
+            foreach (var item in input)
+            {
+                list.AddLast(item);
+            }
+            Console.Write("Enter the index to return from last: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            string result = nthElementFromEnd.ElementFromEnd(list, index);
+            Console.WriteLine(result);
+        }
+    }
+}
