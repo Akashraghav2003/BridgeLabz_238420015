8d5f28cbf90e968b8d3a3dad5a5103d692999631
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:34:58 +0530

    WordFrrequencyCounter add file to count the frequency of word in the text file

---
+++ WordFrequencyCounter/CountFrequency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordFrequencyCounter
+{
+    internal class CountFrequency
+    {
+        public static Dictionary<string, int> CountWordFrequency(string filePath)
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File not found!");
+                return wordCount;
+            }
+
+            string text = File.ReadAllText(filePath).ToLower();
+            string[] words = Regex.Split(text, @"\W+"); // Split by non-word characters
+
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    if (wordCount.ContainsKey(word))
+                        wordCount[word]++;
+                    else
+                        wordCount[word] = 1;
+                }
+            }
+            return wordCount;
+        }
+
+    }
+}
---
+++ WordFrequencyCounter/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace WordFrequencyCounter
+{
+	class Program
+	{
+        static void Main()
+        {
+            Console.Write("Enter file path: ");
+            string filePath = Console.ReadLine();
+
+            Dictionary<string, int> wordFrequencies =CountFrequency.CountWordFrequency(filePath);
+
+            Console.WriteLine("Word Frequencies:");
+            foreach (var pair in wordFrequencies)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
