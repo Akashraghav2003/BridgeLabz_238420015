7f1310c164da31ce2a9885379c4c9c36d8fff3a8
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:18:25 +0530

    FindSubsets add file to check set is subset of another or not

---
+++ FindSubsets/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FindSubsets
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            SubsetFinder finder = new SubsetFinder();
+            Console.WriteLine("Enter the element of set: ");
+            HashSet<int> set1 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            Console.WriteLine("Enter the element of subSet: ");
+            HashSet<int> set2 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            bool result  = finder.CheckSubset(set1, set2);
+
+            if (result) Console.WriteLine("set 2 is sub set of set1");
+            else Console.WriteLine("set2 is not sub set of set1");
+        }
+    }
+}
---
+++ FindSubsets/SubsetFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSubsets
+{
+    internal class SubsetFinder
+    {
+        public bool CheckSubset(HashSet<int> set1, HashSet<int> set2)
+        {
+            if(set1.Count < set2.Count) return false;
+
+            bool flag = true;
+            foreach(int i in set2)
+            {
+                if (!set1.Contains(i)) 
+                { 
+                    flag = false; 
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
