7b741e353d22e8813365e906cb6ed701e3206dfb
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:29:46 +0530

    ShoppingCart add file to managment of sopping

---
+++ Shopping Cart/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Shopping_Cart
+{
+	class Program
+	{
+
+        static void Main()
+        {
+            ShopingCart cart = new ShopingCart();
+
+            cart.AddProduct("Laptop", 1000);
+            cart.AddProduct("Mouse", 50);
+            cart.AddProduct("Keyboard", 75);
+            cart.AddProduct("Monitor", 300);
+
+            cart.DisplayCart();
+        }
+    }
+}
---
+++ Shopping Cart/ShopingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart
+{
+    internal class ShopingCart
+    {
+        private Dictionary<string, double> productPrices = new Dictionary<string, double>();
+        private LinkedList<string> cartOrder = new LinkedList<string>();
+        private SortedDictionary<string, double> sortedCart = new SortedDictionary<string, double>();
+
+        public void AddProduct(string product, double price)
+        {
+            productPrices[product] = price;
+            sortedCart[product] = price;
+            cartOrder.AddLast(product);
+        }
+
+        public void DisplayCart()
+        {
+            Console.WriteLine("\nCart in Order of Addition:");
+            foreach (var product in cartOrder)
+                Console.WriteLine($"{product}: ${productPrices[product]:0.00}");
+
+            Console.WriteLine("\nCart Sorted by Price:");
+            foreach (var item in sortedCart.OrderBy(x => x.Value))
+                Console.WriteLine($"{item.Key}: ${item.Value:0.00}");
+        }
+    }
+}
