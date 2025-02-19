2145f006f1ad31aff26082991a42ad932da25d43
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:29:03 +0530

    RotateElement add file to rotate the element of list by index

---
+++ RotateElement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace RotateElement
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            RotateElementInList rotateElementInList = new RotateElementInList();    
+            Console.WriteLine("Enter the element of list with space: ");
+            string[] inputArray = Console.ReadLine().Split(' ');
+            List<int> list = new List<int>();   
+            foreach (string input in inputArray)
+            {
+                if (int.TryParse(input, out int value))
+                {
+                    list.Add(value);
+                }
+            }
+            Console.Write("Enter the index to rotate: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            List <int> results = rotateElementInList.Rotate(list, index);
+
+            foreach(var result in results)
+            {
+                Console.Write(result+ " ");
+            }
+        }
+    }
+}
---
+++ RotateElement/RotateElementInList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateElement
+{
+    internal class RotateElementInList
+    {
+        public List <int> Rotate(List<int> list, int index)
+        {
+            List<int> result = new List<int>(); 
+            
+            for (int i = index;i< list.Count;i++)
+            {
+                result.Add(list[i]);
+            }
+
+            for(int i =0;i < index; i++)
+            {
+                result.Add(list[i]);
+            }
+
+            return result;
+        }
+    }
+}
