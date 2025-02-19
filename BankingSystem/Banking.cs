be4cf7e314703c4eacd8a11b1ef31c61c65809bf
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:14:14 +0530

    BankingSystem add file to use of stack queue and hashset for implement of Banking System

---
+++ BankingSystem/Banking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    internal class Banking
+    {
+        private Dictionary<int, double> accountBalances = new Dictionary<int, double>();
+        private SortedDictionary<int, double> sortedAccounts = new SortedDictionary<int, double>();
+        private Queue<(int, double)> withdrawalQueue = new Queue<(int, double)>();
+
+        public void AddAccount(int accountNumber, double balance)
+        {
+            accountBalances[accountNumber] = balance;
+            sortedAccounts[accountNumber] = balance;
+        }
+
+        public void WithdrawRequest(int accountNumber, double amount)
+        {
+            if (accountBalances.ContainsKey(accountNumber))
+                withdrawalQueue.Enqueue((accountNumber, amount));
+            else
+                Console.WriteLine($"Account {accountNumber} not found.");
+        }
+
+        public void ProcessWithdrawals()
+        {
+            while (withdrawalQueue.Count > 0)
+            {
+                var (account, amount) = withdrawalQueue.Dequeue();
+                if (accountBalances[account] >= amount)
+                {
+                    accountBalances[account] -= amount;
+                    sortedAccounts[account] = accountBalances[account];
+                    Console.WriteLine($"Withdrawal of ${amount:0.00} from Account {account} successful.");
+                }
+                else
+                {
+                    Console.WriteLine($"Insufficient balance in Account {account}.");
+                }
+            }
+        }
+        public void DisplaySortedAccounts()
+        {
+            Console.WriteLine("\nAccounts Sorted by Balance:");
+            foreach (var account in sortedAccounts.OrderBy(a => a.Value))
+                Console.WriteLine($"Account {account.Key}: ${account.Value:0.00}");
+        }
+    }
+}
---
+++ BankingSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BankingSystem
+{
+	class Program
+	{
+        static void Main()
+        {
+            Banking bank = new Banking();
+
+            bank.AddAccount(101, 1500);
+            bank.AddAccount(102, 500);
+            bank.AddAccount(103, 2000);
+
+            bank.WithdrawRequest(101, 300);
+            bank.WithdrawRequest(102, 600);
+            bank.WithdrawRequest(103, 1000);
+
+            bank.ProcessWithdrawals();
+            bank.DisplaySortedAccounts();
+        }
+    }
+}
