e5a48b44d141457322f3827e817837b589ed22cf
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:20:38 +0530

    FrequencyOfElemnt add file to count frequency of element in the list

---
+++ FrequencyOfElements/Frequency.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyOfElements
+{
+    internal class Frequency
+    {
+        public  Dictionary<string, int > Occurance(List <String> list)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                
+                if (dict.ContainsKey(list[i]))
+                {
+                    dict[list[i]]++;
+
+                }
+                else
+                {
+                    dict.Add(list[i],1);
+                }
+            }
+            return dict;
+        }
+    }
+}
---
+++ FrequencyOfElements/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FrequencyOfElements
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Enter the element of list:");
+            string[] input = Console.ReadLine().Split(' ');
+
+			List<string> list = input.ToList();
+
+			Frequency frequency = new Frequency();
+			 Dictionary<string, int> dict = frequency.Occurance(list);
+
+			foreach(var item in dict)
+			{
+				Console.WriteLine($"{item.Key}: {item.Value}");
+			}
+
+
+        }
+	}
+}
