dc57413cb5c5d62efcf820ccb2d68a372b299201
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:16:16 +0530

    CheckTwoSetsAreEqual add file to check two set are equal or not

---
+++ CheckTwoSetsAreEqual/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CheckTwoSetsAreEqual
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            TwoSetEqual twoSetEqual = new TwoSetEqual();
+            Console.WriteLine("Enter the element for set1 : ");
+            SortedSet<int> set1 = new SortedSet<int>(Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse));
+
+            Console.WriteLine("Enter the element for set2: ");
+            SortedSet<int> set2 = new SortedSet<int>(Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse));
+
+            bool result  = twoSetEqual.EqualSet(set1, set2);
+
+            if (result) Console.WriteLine("Both set are equal");
+            else Console.WriteLine("Sets are not equal.");
+
+        }
+    }
+}
---
+++ CheckTwoSetsAreEqual/TwoSetEqual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckTwoSetsAreEqual
+{
+    internal class TwoSetEqual
+    {
+        public bool EqualSet(SortedSet<int> set1, SortedSet<int> set2)
+        {
+
+            if (set1.Count != set2.Count )return false;
+            bool flag = true;
+            foreach ( int i in set1)
+            {
+                if (! set2.Contains(i))
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
