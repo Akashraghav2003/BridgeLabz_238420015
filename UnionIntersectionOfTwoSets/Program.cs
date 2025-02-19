b23b98f03ef7c1d5bc900df119c57190c93daada
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:32:09 +0530

    UnionIntersectionOfTwoSets add file to find the union and intersection of two set

---
+++ UnionIntersectionOfTwoSets/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace UnionIntersectionOfTwoSets
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            UnionAndIntersection unionAndIntersection = new UnionAndIntersection(); 
+            Console.WriteLine("Enter the element of set1: ");
+            HashSet<int> set1 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            Console.WriteLine("Enter the element of set2: ");
+            HashSet<int> set2 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            HashSet<int> result1 = unionAndIntersection.Union(set1, set2);
+            Console.Write("Union: ");
+            foreach(int x in result1) Console.Write( x + " ");
+            Console.WriteLine();
+            Console.WriteLine("Enter the element of set1: ");
+            HashSet<int> setI1 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            Console.WriteLine("Enter the element of set2: ");
+            HashSet<int> setI2 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+            Console.Write("Intersection: ");
+            HashSet <int> result2  = unionAndIntersection.InterSection(setI1, setI2);
+
+            foreach (int x in result2) Console.Write( x + " ");
+        }
+    }
+}
---
+++ UnionIntersectionOfTwoSets/UnionAndIntersection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnionIntersectionOfTwoSets
+{
+    internal class UnionAndIntersection
+    {
+        public HashSet<int> Union(HashSet<int> set1, HashSet<int> set2)
+        { 
+
+            foreach (int i in set1)
+            {
+                if (!set2.Contains(i))
+                {
+                    set2.Add(i);
+                }
+            }
+
+            return set2;
+        }
+
+        public HashSet<int> InterSection(HashSet<int> set1, HashSet<int> set2)
+        {
+            HashSet<int> result = new HashSet<int>();
+            foreach (int i in set1)
+            {
+                if (set2.Contains(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
