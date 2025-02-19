356dabecfabef719ac6126cdbab1a0adb2c22bcf
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:27:57 +0530

    ReverseQueue add file to reverse the queue with stack

---
+++ ReverseQueue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace ReverseQueue
+{
+	class MyClass
+	{
+        static void Main(string[] args)
+        {
+            ReverseingTheQueue reverseingTheQueue = new ReverseingTheQueue();
+            Console.WriteLine("Enetr the element of queue: ");
+            Queue input = new Queue(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            reverseingTheQueue.Reverse(input);
+
+            Console.Write("Queue after reversing: ");
+            foreach(var item in input) Console.Write(item+" ");
+
+
+
+        }
+    }
+}
---
+++ ReverseQueue/ReverseingTheQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseQueue
+{
+    internal class ReverseingTheQueue
+    {
+        public void Reverse(Queue queue)
+        {
+            Stack  stack = new Stack();
+            while (queue.Count > 0)
+            {
+                var temp = queue.Dequeue();
+                stack.Push(temp);
+            }
+            while (stack.Count > 0)
+            {
+                var temp  = stack.Pop();
+                queue.Enqueue(temp);
+            }
+        }
+    }
+}
