6e0727c159fceb0945ce7849d6f7badd7e906253
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:30:58 +0530

    SymmerticDifference add file to find Symmertic Difference of two set

---
+++ SymmetricDifference/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SymmetricDifference
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            SymmetricDifferenceOfSet symmetricDifferenceOfSet = new SymmetricDifferenceOfSet();     
+            Console.WriteLine("Enter the element of set1: ");
+            HashSet<int> set1 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            Console.WriteLine("Enter the element of set2: ");
+            HashSet<int> set2 = new HashSet<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+
+            HashSet<int> result = symmetricDifferenceOfSet.DifferenceOfSet(set1, set2);
+            Console.Write("Symmetric Difference of Set1 And Set2 ");
+            foreach (int x in result) Console.Write(x+ " ");
+        }
+    }
+}
---
+++ SymmetricDifference/SymmetricDifferenceOfSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricDifference
+{
+    internal class SymmetricDifferenceOfSet
+    {
+        public HashSet<int> DifferenceOfSet(HashSet<int> set1, HashSet<int> set2)
+        {
+            foreach (var item in set1)
+            {
+                if(! set2.Contains(item))
+                {
+                    set2.Add(item);
+                }
+            }
+            return set2;
+        }
+    }
+}
