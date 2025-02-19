cfd9e2f61f4d5250bb567ecd7c7e430d51c2a954
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-19 22:25:59 +0530

    RemoveDuplicatesWhilePreservingOrder add file to remove the duplicate by preserving order

---
+++ RemoveDuplicatesWhilePreservingOrder/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RemoveDuplicatesWhilePreservingOrder
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            RemoveDuplicates removeDuplicates = new RemoveDuplicates(); 
+            Console.WriteLine("Enter the element of list with space: ");
+            List<int> input = new List<int>(Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse));
+
+            removeDuplicates.Remove(input);
+
+            foreach (int i in input)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
---
+++ RemoveDuplicatesWhilePreservingOrder/RemoveDuplicates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicatesWhilePreservingOrder
+{
+    internal class RemoveDuplicates
+    {
+        public void Remove(List<int> list)
+        {
+            HashSet<int> set = new HashSet<int>();
+            int index = 0;
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (set.Contains(list[i]))
+                {
+                   list[index] = list[i];
+                    index++;
+                }
+                else
+                {
+                    set.Add(list[i]);
+                }
+            }
+            while(set.Count < list.Count)
+            {
+                list.RemoveAt(list.Count - 1);
+            }
+        }
+    }
+}
