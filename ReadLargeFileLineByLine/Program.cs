2a1020ee64781d1c38a06886bbbd8b72a3e2c9ac
Akashraghav2003 <akash.si8273@gmail.com>
2025-02-20 16:50:31 +0530

    ReadLargeFileLineByLine add file to read and write the file by stream reader

---
+++ ReadLargeFileLineByLine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"D:\akash gla university\capgemini problem\feature20-02-2025\ReadLargeFileLineByLine\largeFile.txt"; 
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.IndexOf("error", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
