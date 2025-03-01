ff82de5cdb1b4c8a63780274b344c79afc0e8aa0
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 13:47:55 +0530

    ParametrizeTest add file to test the even number by given the different testcase in Parametrize file

---
+++ Parameterized/EvenCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameterized
+{
+    public class EvenCheck
+    {
+        public bool IsEven(int num)
+        {
+            bool flag = false;
+
+            if (num % 2 == 0)
+            {
+                flag = true;
+            }
+            return flag;
+        }
+    }
+}
---
+++ ParameterizedTest/EvenCheckTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Parameterized;
+
+namespace ParameterizedTest
+{
+    [TestFixture]
+    public class EvenCheckTest
+    {
+        private int num;
+        private EvenCheck evenCheck;
+
+        [SetUp]
+        public void SetUp()
+        {
+            num = 0;
+            evenCheck = new EvenCheck();
+        }
+        [Test]
+        [TestCase(2, ExpectedResult = true)]
+        [TestCase(4, ExpectedResult = true)]
+        [TestCase(6, ExpectedResult = true)]
+        [TestCase(7, ExpectedResult = false)]
+        [TestCase(9, ExpectedResult = false)]
+        public bool IsEven_TestCases(int number)
+        {
+            return evenCheck.IsEven(number);
+        }
+
+    }
+}
