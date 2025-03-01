925f8c1fd83cb8e462fc2c13eac51191239cff1c
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 14:05:57 +0530

    ValidateSSN add file to validaet the social security number

---
+++ Validate a Social Security Number (SSN)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static bool ValidateSSN(string ssn)
+    {
+        string pattern = @"^\d{3}-\d{2}-\d{4}$"; // Matches "123-45-6789"
+        return Regex.IsMatch(ssn, pattern);
+    }
+
+    static void Main()
+    {
+        string[] testSSNs = {
+            "123-45-6789",  
+            "123456789",    
+            "12-345-6789",  
+            "000-00-0000",  
+            "987-65-4321"   
+        };
+
+        foreach (var ssn in testSSNs)
+        {
+            Console.WriteLine($"{ssn} → {(ValidateSSN(ssn) ? "Valid" : "Invalid")}");
+        }
+    }
+}
