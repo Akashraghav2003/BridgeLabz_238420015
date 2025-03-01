e762285c65e5b909176d59c0ada93461b5b19bef
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 12:05:43 +0530

    HandlingFileNotFoundException add file to handle exception when not to find file

---
+++ HandlingFileNotFoundException/FileHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingFileNotFoundException
+{
+    internal class FileHandling
+    {
+        public void ReadFile(string path)
+        {
+            try
+            {
+                if(File.Exists(path))
+                {
+                    string Content = File.ReadAllText(path);
+                    Console.WriteLine(Content);
+                }
+                else
+                {
+                    throw new IOException("File not found");
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ HandlingFileNotFoundException/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HandlingFileNotFoundException
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            FileHandling fh = new FileHandling();
+            string path = @"D:\akash gla university\capgemini problem\feature21-02-2025\HandlingFileNotFoundException\new file.txt";
+
+            try
+            {
+                fh.ReadFile(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
