d4a4f00fd9d20155ad3ffa1074260dc24eed2ea0
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 21:13:37 +0530

    ReadCSVfile add to read the CSV file to read the CSV file

---
+++ ReadCSVFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ReadCSVFile
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            string path = @"D:\akash gla university\capgemini problem\feature25-02-2025\Employee.csv";
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
