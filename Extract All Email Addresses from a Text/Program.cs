14bf97e4c090fecfcd4582234486efac858d0cb9
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 20:47:26 +0530

    Extract All Email Addresses from a Text add file to extract email from text

---
+++ Extract All Email Addresses from a Text/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void ExtractEmails(string text)
+    {
+        string pattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Value);
+        }
+    }
+
+    static void Main()
+    {
+        string inputText = "Contact us at support@example.com and info@company.org  and ishwarmars@gmail.com for more details.";
+
+        Console.WriteLine("Extracted Email Addresses:");
+        ExtractEmails(inputText);
+    }
+}
