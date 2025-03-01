5dd80ce20abadfcc7d718d5d59e56e29f2777d53
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 13:54:17 +0530

    StringUtilityTest add file to test the operation of string in striingUtility File

---
+++ StringUtility/StringUtils.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtility
+{
+    public class StringUtils
+    {
+        public string str { get; set; }
+
+        public StringUtils(string str)
+        {
+            this.str = str;
+        }
+
+        public string Reverse()
+        {
+            string result = "";
+            
+            int j =str.Length-1;
+            while (j>=0)
+            {
+                result += str[j];
+                j--;
+
+            }
+            return result;
+        }
+        public bool IsPalindrome()
+        {
+            bool flag = true;
+            int j =str.Length-1, i=0;
+            while (i<j)
+            {
+                if (str[i] != str[j])
+                {
+                    flag = false; 
+                    break;
+                }
+                i++;
+                j--;
+            }
+            return flag;
+        }
+
+        public string ToUpperCase()
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                // Convert only lowercase letters ('a' to 'z') to uppercase
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    result += (char)(ch - 32);
+                }
+                else
+                {
+                    result += ch; // Keep other characters unchanged
+                }
+            }
+            return result;
+        }
+
+
+
+    }
+}
---
+++ StringUtilityTest/StringUtilsTest.cs
@@ -0,0 +1,64 @@
+﻿using StringUtility;
+using NUnit.Framework;
+
+
+namespace StringUtilityTest
+{
+    [TestFixture]
+    public class StringUtilsTest
+    {
+        private StringUtils stringUtils;
+        [SetUp]
+        public void SetUp()
+        {
+            stringUtils = new StringUtils("aaaaaaa");
+        }
+        [Test]
+        public void Reverse_String_ReturnReverse()
+        {
+            stringUtils.str = "akash";
+            string result  = stringUtils.Reverse();
+            Assert.AreEqual("hsaka", result);
+        }
+
+        [Test]
+        public void Reverse_String_ReturnIrReverse()
+        {
+            stringUtils.str = "akash";
+            string result = stringUtils.Reverse();
+            Assert.AreNotEqual("akash", result);
+        }
+        [Test]
+        public void IsPalindrome_string_returnIsTrue()
+        {
+            stringUtils.str = "aaaaaaa";
+            bool result = stringUtils.IsPalindrome();
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void IsPalindrome_string_returnIsFalse()
+        {
+            stringUtils.str = "asdfgh";
+            bool result = stringUtils.IsPalindrome();
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void ToUpper_string_returnUpperString()
+        {
+            stringUtils.str = "akashraghav";
+            string result = stringUtils.ToUpperCase();
+            Assert.AreEqual("AKASHRAGHAV", result);
+        }
+        [Test]
+        public void ToUpper_string_returnNotUpperString()
+        {
+            stringUtils.str = "akashraghav";
+            string result = stringUtils.ToUpperCase();
+            Assert.AreNotEqual("AKAShRaghav", result);
+        }
+
+
+
+
+    }
+}
