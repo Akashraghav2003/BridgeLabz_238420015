a84f77e84f5437a7afab25b55536ec263ac1aad3
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 13:45:33 +0530

    ListOperationTest add file to test the ListPoeration file

---
+++ ListOperationTest/ListManagerTest.cs
@@ -0,0 +1,83 @@
+﻿using ListOperations;
+using NUnit.Framework;
+
+
+namespace ListOperationTest
+{
+    [TestFixture]
+    public class ListManagerTest
+    {
+        private List<int> testlist;
+        private ListManager listManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testlist = new List<int>();
+            listManager = new ListManager();
+        }
+
+        [Test]
+        public void AddElement_List_ReturnList()
+        {
+            listManager.AddElements(testlist, 10);
+            Assert.Contains(10, testlist);
+        }
+        [Test]
+        public void RemoveElement_RemoveElementFromList()
+        {
+            testlist.Add(5);
+
+            bool result = listManager.RemoveElemnt(testlist, 5);
+
+            Assert.IsTrue(result);
+            Assert.IsFalse(testlist.Contains(5));
+        }
+        [Test]
+        public void RemoveElement_RemoveElementFromListNotContains_ReturnFalse()
+        {
+            testlist.Add(5);
+
+            bool result = listManager.RemoveElemnt(testlist, 3);
+
+            Assert.IsFalse(result);
+            Assert.IsTrue(testlist.Contains(5));
+        }
+
+        [Test]
+        public void GetSized_ReturnSizeOfList()
+        {
+            testlist.Add(5);
+            testlist.Add(7);
+            testlist.Add(8);
+            testlist.Add(5);
+
+            int size = listManager.GetSize(testlist);
+
+
+            Assert.AreEqual(4, size);
+        }
+        [Test]
+        public void GetSized_ReturnSizeOfListafterRemovel()
+        {
+            testlist.Add(5);
+            testlist.Add(7);
+            testlist.Add(8);
+            testlist.Add(5);
+
+            listManager.RemoveElemnt(testlist, 5);
+            listManager.RemoveElemnt(testlist, 7);
+
+            int size = listManager.GetSize(testlist);
+            Assert.AreEqual(2, size);
+        }
+        [Test]
+
+        public void RemoveElement_ThrowArgumentalNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => listManager.RemoveElemnt(null, 5));
+        }
+        
+
+    }
+}
---
+++ ListOperations/ListManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOperations
+{
+    public class ListManager
+    {
+        
+        public List<int> AddElements(List<int> list, int element)
+        {
+            list.Add(element);
+            return list;
+        }
+
+        public bool RemoveElemnt(List<int> list, int element)
+        {
+            if(list == null)
+                throw new ArgumentNullException(nameof(list), "List can not be null.");
+
+            return list.Remove(element);
+        }
+
+        public int GetSize(List<int> list)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list), "List can not be empty");
+
+            return list.Count;
+        }
+    
+    }
+}
