44511ee3ecc8e7f486bd80ac50a487b2bd2168dd
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 21:06:57 +0530

    FilterRecords add file to filter the CSV file

---
+++ FilterRecords/Program.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
+
+namespace FilterRecords
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            try 
+            {
+                string path = "student_marks1.csv";
+
+                if (!File.Exists(path))
+                {
+                    throw (new FileNotFoundException());
+                }
+
+                using(var reader = new StreamReader(path))
+                using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+
+                    while (csv.Read())
+                    {
+                        double marks = csv.GetField<double>("Average Marks");
+                        if (marks >= 80)
+                        {
+                            string name = csv.GetField("Name");
+                            string gender = csv.GetField("Gender");
+
+                            Console.WriteLine($"Name: {name}, Gender: {gender}, Marks: {marks}");
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
