303214558246ba95d6fd0ca379ddf05ce3eeda5e
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 12:03:39 +0530

    Demonstrating finally Block Execution add file to use try catch and finally in add and division program

---
+++ DemonstratingFinallyBlockExecution/Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemonstratingFinallyBlockExecution
+{
+    internal class Division
+    {
+        public double divide(double divisor, double divisor2) { }
+        {
+            
+        }
+    }
+}
