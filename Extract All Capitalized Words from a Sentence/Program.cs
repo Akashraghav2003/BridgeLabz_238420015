ce9d453da58f48099f02b643e042a36e99eb1b78
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 20:50:19 +0530

    Extract All Capitalized Words from a Sentence add file to extract Capital word from text

---
+++ Extract All Capitalized Words from a Sentence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class Program
+{
+    static void ExtractCapitalizedWords(string text)
+    {
+        string pattern = @"\b[A-Z][a-z]*\b";
+
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        string[] capitalizedWords = matches.Cast<Match>().Select(m => m.Value).ToArray();
+
+        Console.WriteLine(string.Join(", ", capitalizedWords));
+    }
+
+    static void Main()
+    {
+        string inputText = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+
+        Console.WriteLine("Extracted Capitalized Words:");
+        ExtractCapitalizedWords(inputText);
+    }
+}
