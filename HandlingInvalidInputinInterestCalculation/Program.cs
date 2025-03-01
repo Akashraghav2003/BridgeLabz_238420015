bace784e1cefc07dd6df67c6ceda0ce3b8f6266f
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 12:06:49 +0530

    HandlingInvalidInputinInterestCalculation add file to handle the format error when take input from user

---
+++ HandlingInvalidInputinInterestCalculation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HandlingInvalidInputinInterestCalculation
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the amount: ");
+                double amount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the rate: ");
+                double rate = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+               double result =  SimpleIntrest.Intrest(amount, rate, year);
+
+                Console.WriteLine("Intrest after {year} year is:" + result);
+            }
+            catch (SimpleIntrest ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ HandlingInvalidInputinInterestCalculation/SimpleIntrest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingInvalidInputinInterestCalculation
+{
+    internal class SimpleIntrest : Exception
+    {
+        public  SimpleIntrest(string msg) : base(msg) { }
+        public static  double Intrest(double amount, double rate, int year)
+        {
+            if (amount < 0 || rate < 0)
+            {
+                throw new SimpleIntrest(" amount and rate is negative");
+            }
+            double si = (amount * rate * year) / 100.0;
+            return si;  
+        }
+    }
+
+}
