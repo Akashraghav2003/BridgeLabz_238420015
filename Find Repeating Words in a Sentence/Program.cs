e33490e8994944286d09e772b310b840fbe839b3
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 14:11:38 +0530

    Find Repeating Words in a Sentence add file to find repeting word in sentence

---
+++ Find Repeating Words in a Sentence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class Program
+{
+    static void FindRepeatingWords(string text)
+    {
+        string pattern = @"\b(\w+)\s+\1\b"; // Captures repeated words
+        MatchCollection matches = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
+
+        string[] repeatedWords = matches.Cast<Match>().Select(m => m.Groups[1].Value).Distinct().ToArray();
+
+        Console.WriteLine(string.Join(", ", repeatedWords));
+    }
+
+    static void Main()
+    {
+        string inputText = "This is is a repeated repeated word test.";
+
+        Console.WriteLine("Repeating Words:");
+        FindRepeatingWords(inputText);
+    }
+}
