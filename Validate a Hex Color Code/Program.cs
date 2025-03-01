d3b5026a353874ce1d3dee856600f0e2d1c880f8
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 14:08:04 +0530

    Validate a Hex Color Code add file to validate the hexa color code

---
+++ Validate a Hex Color Code/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static bool IsValidHexColor(string color)
+    {
+        string pattern = @"^#([A-Fa-f0-9]{6})$";
+        return Regex.IsMatch(color, pattern);
+    }
+
+    static void Main()
+    {
+        string[] testColors = { "#FFA500", "#ff4500", "#123", "#zzzzzz", "#abcdef", "#123456" };
+
+        foreach (var color in testColors)
+        {
+            Console.WriteLine($"Hex Color: {color}, Valid: {IsValidHexColor(color)}");
+        }
+    }
+}
