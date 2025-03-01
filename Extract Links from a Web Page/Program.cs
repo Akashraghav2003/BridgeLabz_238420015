62b4066dd0dba621c77c2f672ac6b91b5cfffe40
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 20:39:07 +0530

    Extract Links from a Web Page add file to extract link from a file

---
+++ Extract Links from a Web Page/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class Program
+{
+    static void ExtractLinks(string text)
+    {
+        string pattern = @"\bhttps?:\/\/[^\s/$.?#].[^\s]*";
+
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        string[] links = matches.Cast<Match>().Select(m => m.Value).ToArray();
+
+        Console.WriteLine(string.Join(", ", links));
+    }
+
+    static void Main()
+    {
+        string inputText = "Visit https://www.google.com and http://example.org for more info.";
+
+        Console.WriteLine("Extracted Links:");
+        ExtractLinks(inputText);
+    }
+}
---
+++ Extract Programming Language Names from a Text/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class Program
+{
+    static void ExtractProgrammingLanguages(string text)
+    {
+        string[] languages = { "Java", "Python", "JavaScript", "C", "C++", "C#", "Go", "Swift", "Ruby", "Kotlin", "PHP", "Rust", "TypeScript" };
+        string pattern = @"\b(" + string.Join("|", languages.Select(Regex.Escape)) + @")\b";
+
+        MatchCollection matches = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
+        string[] extractedLanguages = matches.Cast<Match>().Select(m => m.Value).Distinct().ToArray();
+
+        Console.WriteLine(string.Join(", ", extractedLanguages));
+    }
+
+    static void Main()
+    {
+        string inputText = "I love Java, Python, and JavaScript, but I haven't tried Go yet.";
+
+        Console.WriteLine("Extracted Programming Languages:");
+        ExtractProgrammingLanguages(inputText);
+    }
+}
