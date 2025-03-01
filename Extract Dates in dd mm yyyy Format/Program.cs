7dfc0c1dd75f27d4ac5f518229eb7bf1d8bd0aae
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 20:42:14 +0530

    Extract Dates in dd mm yyyy Format add file to extract date from the file

---
+++ Extract Dates in dd mm yyyy Format/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class Program
+{
+    static void ExtractDates(string text)
+    {
+        string pattern = @"\b(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}\b";
+
+        MatchCollection matches = Regex.Matches(text, pattern);
+
+        string[] dates = matches.Cast<Match>().Select(m => m.Value).ToArray();
+
+        Console.WriteLine(string.Join(", ", dates));
+    }
+
+    static void Main()
+    {
+        string inputText = "The events are scheduled for 12/05/2023, 16/04/2024, and 29/02/2020.";
+
+        Console.WriteLine("Extracted Dates:");
+        ExtractDates(inputText);
+    }
+}
