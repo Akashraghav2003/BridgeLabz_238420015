6f89cb90bac1f18f5bf4140fbbf91412964a480d
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 12:15:20 +0530

    ValidateaTheAge add to validaet the age of user to vote

---
+++ ValidateaTheAge/AgeCheck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateaTheAge
+{
+    internal class AgeCheck: Exception
+    {
+        public AgeCheck(string msg): base(msg){ }
+        public static void CheckError(int age)
+        {
+            if (age < 18) { throw new AgeCheck("Invalid age exception: age sholud be greater then 18"); }
+        }
+    }
+}
---
+++ ValidateaTheAge/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ValidateaTheAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Enter the age of person: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                AgeCheck.CheckError(age);
+
+
+            }
+            catch (AgeCheck ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
