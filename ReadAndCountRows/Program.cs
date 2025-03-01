09d051ba471a4d096be832c675d461c17049a1a4
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 21:12:39 +0530

    ReadAndCountRows add file to read and count rows of CSV file

---
+++ ReadAndCountRows/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.IO;
+using CsvHelper.Configuration.Attributes;
+using System.Globalization;
+
+namespace ReadCSV
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            string path = @"D:\akash gla university\capgemini problem\feature25-02-2025\Employee.csv";
+
+            int rows = 0;
+
+            using(var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csv.Read();
+                csv.ReadHeader();
+
+                while (csv.Read())
+                {
+                    rows++;
+                }
+            }
+
+            Console.WriteLine($"Total number of Rows: {rows}");
+        }
+    }
+}
