17b09b5a5ce0960980f66f3f54adf245e99a35a6
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 13:51:27 +0530

    PerformanceTest add file to test the performance of task in Performance file

---
+++ Performance/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Performance
+{
+    public class TaskManager
+    {
+        public string RunningTask(int sleepTime)
+        {
+            Thread.Sleep(sleepTime);
+            return "Task is complete";
+        }
+    }
+}
---
+++ PerformanceTest/TaskManagerTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Performance;
+
+
+
+namespace PerformanceTest
+{
+    [TestFixture]
+    public class TaskManagerTest
+    {
+        private int sleepTime;
+        private TaskManager taskManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            taskManager = new TaskManager();
+        }
+
+        [Test, Timeout(2000)]
+        public void RunningTask_ShouldFailedDueToTimeOut()
+        {
+            sleepTime = 3000;
+            string result = taskManager.RunningTask(sleepTime);
+
+            Assert.AreEqual("Task is complete", result);
+        }
+        [Test, Timeout(2000)]
+        public void RunningTask_ShouldPassedWithoutTimeOut()
+        {
+            sleepTime = 1500;
+            string result = taskManager.RunningTask(sleepTime);
+
+            Assert.AreEqual("Task is complete", result);
+        }
+    }
+}
