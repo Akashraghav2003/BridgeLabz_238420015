9178a49cd9624f864e79b8cc0f4f90647447aed6
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 12:22:23 +0530

    DateUtilitiesTest add file to test the file named DateUtilities

---
+++ DateUtilities/DateFormatter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace DateUtilities
+{
+    public class DateFormatter
+    {
+        // Converts date from "yyyy-MM-dd" to "dd-MM-yyyy"
+        public string FormatDate(string inputDate)
+        {
+            if (string.IsNullOrWhiteSpace(inputDate))
+                throw new ArgumentException("Input date cannot be null or empty.");
+
+            DateTime date;
+            if (DateTime.TryParseExact(inputDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+            {
+                return date.ToString("dd-MM-yyyy");
+            }
+            else
+            {
+                throw new FormatException("Invalid date format. Expected format: yyyy-MM-dd");
+            }
+        }
+    }
+}
---
+++ DateUtilitiesTest/Class1.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using DateUtilities;
+using System;
+
+namespace DateUtilitiesTest
+{
+    [TestFixture]
+    public class DateFormatterTest
+    {
+        private DateFormatter dateFormatter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dateFormatter = new DateFormatter();
+        }
+
+        [Test]
+        public void FormatDate_ValidDate_ShouldReturnFormattedDate()
+        {
+            string result = dateFormatter.FormatDate("2024-02-22");
+            Assert.AreEqual("22-02-2024", result, "2024-02-22 should convert to 22-02-2024");
+        }
+
+        [Test]
+        public void FormatDate_InvalidDate_ShouldThrowFormatException()
+        {
+            Assert.Throws<FormatException>(() => dateFormatter.FormatDate("22-02-2024"), "Should throw exception for wrong format.");
+        }
+
+        [Test]
+        public void FormatDate_EmptyString_ShouldThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => dateFormatter.FormatDate(""), "Should throw exception for empty input.");
+        }
+
+        [Test]
+        public void FormatDate_NullInput_ShouldThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => dateFormatter.FormatDate(null), "Should throw exception for null input.");
+        }
+    }
+}
