c7c77cf72cefd8f6643d88498fbea660e6eed46c
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 13:42:19 +0530

    ExceptionHandlingTest add file to test the exception handling

---
+++ ExceptionHandling/DivideException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class DivideException
+    {
+        public static double Divide(double divisor, double divident)
+        {
+            if (divident == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            double result = divisor / divident;
+
+            return result;
+        }
+    }
+}
---
+++ ExceptionHandlingTest/DivideExceptionTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using ExceptionHandling;
+
+namespace ExceptionHandlingTest
+{
+    [TestFixture]
+    public class DivideExceptionTest
+    {
+        private double divisor;
+        private double divident;
+
+        [SetUp]
+        public void SetUp()
+        {
+            divisor = 1;
+            divident = 1;
+        }
+
+        [Test]
+        public void Divide_throwDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => DivideException.Divide(10, 0));
+        }
+        [Test]
+        public void Divide_number_ReturnDividedValue()
+        {
+            divident = 10;
+            divisor = 500;
+
+            double result = DivideException.Divide( divisor, divident);
+
+            Assert.AreEqual(50, result);
+        }
+
+
+    }
+}
