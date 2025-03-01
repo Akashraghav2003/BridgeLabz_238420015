2bdd8356d2a4a58efc4a12ed7b17bded102a64b5
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 12:04:46 +0530

    Handling Division Input Error add file to throw the Divide by Zero error

---
+++ HandlingDivisionInputErrors/Division.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingDivisionInputErrors
+{
+    internal class Division
+    {
+        public double divide(double a, double b)
+        {
+            if(b == 0) { throw new DivideByZeroException(); }
+           double c;
+            c = a / b; 
+            return c;  
+
+        }
+    }
+}
---
+++ HandlingDivisionInputErrors/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HandlingDivisionInputErrors
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Division div = new Division(); 
+            try
+            {
+                Console.Write("Enter the divident:");
+                double divident = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the divisor: ");
+                double divisor = Convert.ToDouble(Console.ReadLine());
+
+               double result =  div.divide(divident, divisor);
+
+                Console.WriteLine("Result after divide: " + result );
+            }catch(FormatException)
+            {
+                Console.WriteLine("Enter the Correct datatype value: " );
+            }catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divisor should not be zero");
+            }
+            
+        }
+    }
+}
