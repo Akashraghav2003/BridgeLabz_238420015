b11ca5ed3d48fb304a8199b8814c3c248dc9b0b6
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-01 21:17:14 +0530

    WriteDataToCSVFile add file to wtite the data to CSV file

---
+++ WriteDataToCSVFile/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteData
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Department { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name,string department, double salary)
+        {
+            Id = id;
+            Name = name;
+            Department = department;
+            Salary = salary;
+        }
+
+        public string ToString()
+        {
+            return "Id:" + Id + "";
+        }
+
+    }
+}
---
+++ WriteDataToCSVFile/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.IO;
+using System.Globalization;
+using System.Collections;
+
+namespace WriteData
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            string path = "Employee.csv";
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File does not exits. Creating File...");
+                var employee = new List<Employee>();
+                while(true)
+                {
+                    Console.WriteLine("Choose the operation: ");
+                    Console.WriteLine(" 1.add new employee \n 2.Exits");
+                    int operaqtion = Convert.ToInt32(Console.ReadLine());
+
+                    switch(operaqtion)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the Id: ");
+                            int Id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter the name: ");
+                            string Name = Console.ReadLine();
+                            Console.WriteLine("Enter the Department: ");
+                            string Department = Console.ReadLine();
+                            Console.WriteLine("Enter the Salary:");
+                            double salary = Convert.ToDouble(Console.ReadLine());
+
+                            Employee emplo = new Employee(Id, Name, Department, salary);
+                            employee.Add(emplo);
+                            break;
+                        
+                    }
+                    if (operaqtion == 2)
+                        break;
+                }
+                
+                using (var writer = new StreamWriter(path))
+                using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+                {
+                    csv.WriteRecords(employee);
+                }
+                Console.WriteLine("Add data");
+
+            }
+            else
+            {
+                Console.WriteLine("File allrady exits");
+            }
+           
+        }
+    }
+}
